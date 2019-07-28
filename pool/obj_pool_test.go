060e3203319b17287b4f33cc07a7ffdbd3523b10
yangquan1 <yangquan1@staff.weibo.com>
2019-07-28 23:37:39 +0800

    pool

---
+++ pool/obj_pool_test.go
@@ -0,0 +1,22 @@
+package pool
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestObjPool(t *testing.T) {
+	pool := NewObjPool(10)
+	for i := 0; i < 10; i++ {
+		if v, err := pool.GetObj(time.Second * 1); err != nil {
+			t.Error(err)
+		} else {
+			fmt.Printf("%T\n", v)
+			if err := pool.ReleaseObj(v); err != nil {
+				t.Error(err)
+			}
+		}
+	}
+	fmt.Println("Done")
+}
